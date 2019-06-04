e798c0447a88dfb250e5ba096f18de8f1f4051f4
magjack <jackmagowan94@gmail.com>
2019-06-04 21:46:26 +0100

    Added unit test descriptions

---
+++ src/reducers/game.test.js
@@ -0,0 +1,41 @@
+import {  } from "module";
+
+describe('Testing game reducer', () => {
+
+    it('Places a value into grid array after turn', () => {
+        
+    });
+
+    it('Changes the player from X to O after turn', () => {
+        
+    });
+
+    it('Check if the game is over after 9 goes', () => {
+        
+    });
+
+    it('Resets the game after resubmit action', () => {
+        
+    });    
+
+});
+
+describe('Testing the isThereAWinner function', () => {
+
+    it('Checks for horizontal win', () => {
+        
+    });
+
+    it('Checks for vertical win', () => {
+        
+    });
+
+    it('Check for vertical win', () => {
+        
+    });
+
+    it('Check for stale mate', () => {
+        
+    });
+    
+});
